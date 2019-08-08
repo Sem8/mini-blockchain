eb9a955e5ad811f6a7b85e5ad50c0a6fe3caa30d
Sem8 <mssemmi8@gmail.com>
2019-08-07 23:23:58 -0400

    chain of blocks coded and printed as string and each block referring to previous hash

--- semCoin.py
+++ semCoin.py
@@ -10,9 +10,16 @@ class Block():
     def calcHash(self):
         block_string = json.dumps({"nonce": self.nonce, 'tstamp': self.tstamp, 'transaction': self.transaction, 'prevhash': self.prevhash}, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
-    def printHashes(self):
-        print('prevhash', self.prevhash)
-        print('hash', self.hash)
+    # def printHashes(self):
+    #     print('prevhash', self.prevhash)
+    #     print('hash', self.hash)
+    def __str__(self):
+        string = 'nonce: ' + str(self.nonce) + '\n'
+        string += 'tstamp: ' + str(self.tstamp) + '\n'
+        string += 'transaction: ' + str(self.transaction) + '\n'
+        string += 'prevhash: ' + str(self.prevhash) + '\n'
+
+        return string
 
 # bblock = Block(1, '01/02/2018', 100)
 # bblock.printHashes()
@@ -28,5 +35,13 @@ class BlockChain():
         newBlock.prevhash = self.getLastBlock().hash
         newBlock.hash = newBlock.calcHash()
         self.chain.append(newBlock)
+
+semCoin = BlockChain()
+semCoin.addBlock(Block(1, '05/20/2017', 100))
+semCoin.addBlock(Block(2, '05/21/2017', 20))
+
+for b in semCoin.chain:
+    print(b)
+
 
 
